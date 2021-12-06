fac0b6a4c71d01003c97618ea11d8bb4a901a39f
Jackson Price <59149601+jprice8@users.noreply.github.com>
2021-12-06 17:12:03 -0600

    Complete first three questions in primitive types.

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # result = 0
+    # while x:
+        # result = result * 10 + x % 10
+        # x /= 10
+    
+    # return result
+
+    result, x_remaining = 0, abs(x)
+    while x_remaining:
+        result = result * 10 + x_remaining % 10
+        x_remaining //= 10
+
+    return -result if x < 0 else result
 
 
 if __name__ == '__main__':
